ee324d7c0946928cc7ed2039e87297392fdc5697
Arthur Pereira da Silva <arthurnpdasilva@gmail.com>
2024-12-04 19:25:38 -0300

    app-cmd

---
+++ aplicacao-cmd/app/app.go
@@ -0,0 +1,67 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+func Gerar() *cli.App {
+	app := cli.NewApp()
+	app.Name = "Aplicação de Linha de Comando"
+	app.Usage = "Buscar IPs e Nomes de servidor na internet"
+	//go run main.go ip --host amazon.com.br
+	//go run main.go ip
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "devbook.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Buscar IPs",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Buscar nome de servidores",
+			Flags:  flags,
+			Action: buscarServidores,
+		},
+	}
+
+	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+	//NS = Name Server
+	servers, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
+}
---
+++ aplicacao-cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	application := app.Gerar()
+	if erro := application.Run(os.Args); erro != nil {
+		log.Fatal()
+	}
+}
